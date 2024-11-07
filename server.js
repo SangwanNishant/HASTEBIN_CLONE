0ee60a5075da4a4ae9cb7d32296bbd566af01b60
SangwanNishant <nishantsangwan729@gmail.com>
2024-11-07 13:59:12 +0530

    Completed the hastebin clone

--- server.js
+++ server.js
@@ -1,15 +1,66 @@
-const express = require('express')
+const express = require("express")
+const mongoose = require("mongoose")
+const Document = require("./models/Document")
 const app = express()
 
-app.use(express.static(__dirname + '/public'))
-app.set('view engine', 'ejs')
+require('dotenv').config(); // Load environment variables
 
-app.get('/',(req,res)=>{
-    const code = `Welcome to WasteBin
-here you can write code and
-share that with your friends`
-    res.render('code_display',{code});
+// Connect to MongoDB using the connection string in .env
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(error => console.error('MongoDB connection error:', error));
+
+
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+
+
+
+
+
+app.get("/", (req, res) => {
+  const code = `Welcome to WasteBin!
+
+Use the commands in the top right corner
+to create a new file to share with others.`
+
+  res.render("code_display", { code, language: "plaintext" })
 })
 
+app.get("/new", (req, res) => {
+  res.render("new")
+})
 
-app.listen(3000)
+app.post("/save", async (req, res) => {
+  const value = req.body.value
+  try {
+    const document = await Document.create({ value })
+    res.redirect(`/${document.id}`)
+  } catch (e) {
+    res.render("new", { value })
+  }
+})
+
+app.get("/:id/duplicate", async (req, res) => {
+  const id = req.params.id
+  try {
+    const document = await Document.findById(id)
+    res.render("new", { value: document.value })
+  } catch (e) {
+    res.redirect(`/${id}`)
+  }
+})
+
+app.get("/:id", async (req, res) => {
+  const id = req.params.id
+  try {
+    const document = await Document.findById(id)
+
+    res.render("code_display", { code: document.value, id })
+  } catch (e) {
+    res.redirect("/")
+  }
+})
+
+app.listen(process.env.PORT|| 5000)
